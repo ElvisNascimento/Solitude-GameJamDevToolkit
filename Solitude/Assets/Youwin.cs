11e1a73f5905d456bf970b7fac034324bb9dbb77
ELVIS NASCIMENTO <elvisnascimentodev@gmail.com>
2023-01-22 17:21:42 -0300

    finalizada a game jam jogo jogavel

---
+++ Solitude/Assets/Youwin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Youwin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
